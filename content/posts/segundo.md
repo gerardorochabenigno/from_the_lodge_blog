b9022a130a4e4474405c31d052846a2218b32cbd
gerardorochabenigno <grochabe@itam.mx>
2025-06-12 18:14:43 -0600

    Para despliegue en netlifly con una página de inicio y usando el template de monospace. Hay dos articulos dummies

---
+++ content/posts/segundo.md
@@ -0,0 +1,25 @@
+Title: Una clase para descargar datos del SIE
+Date: 2025-06-12
+Category: General
+Tags: pelican, blog, sie
+Slug: sie-banxico-extractor-python
+Authors: Gerardo Rocha Benigno
+Summary: Como crear un programa en Python que descargue información del Sistema de Información Econónica.
+
+# Como crear un programa que descargue información del SIE!
+
+En este post mostraré como crear un programa de Pyhton que descargue información del Sistema de Información Económica (SIE) de Banco de México, una fuente con información muy valiosa para el análisis de variables macro y microeconómicas, .
+
+## ¿Qué encontrarás aquí?
+
+- **Tutoriales**: Guías para la aplicación de técnicas de Machine Learning y Deep Learning a datos abiertos sobre sistemas de pago .
+- **Noticias**: Actualizaciones sobre el desarrollo de los sistemas de pagos en México.
+- **Opiniones**: Reflexiones sobre temas actuales en México relacionados con los sistemas de pago.
+
+```python
+def hola_mundo():
+    print("¡Hola mundo desde mi blog!")
+
+hola_mundo()
+```
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -16,34 +16,40 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-
 # Widget de redes sociales
 SOCIAL = (
     ('github', 'https://github.com/gerardorochabenigno'),
-    ('linkedin', 'www.linkedin.com/in/gerardorochabenigno'),
-          )
+    ('linkedin', 'https://www.linkedin.com/in/gerardorochabenigno'),
+)
 
 DEFAULT_PAGINATION = 10
 
 THEME = 'themes/monospace'
-
-PLIGIN_PATHS = ['plugins']
-
-PLUGINS = ['sitemap', 'neighbors']
-
-SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.5,
-        'indexes': 0.5,
-        'pages': 0.5
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
     },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly'
-    }
+    'output_format': 'html5',
 }
+
+# Comenta los plugins por ahora
+# PLUGIN_PATHS = ['plugins']
+# PLUGINS = ['sitemap', 'neighbors']
+# SITEMAP = {
+#     'format': 'xml',
+#     'priorities': {
+#         'articles': 0.5,
+#         'indexes': 0.5,
+#         'pages': 0.5
+#     },
+#     'changefreqs': {
+#         'articles': 'monthly',
+#         'indexes': 'daily',
+#         'pages': 'monthly'
+#     }
+# }
 
 STATIC_PATHS = ['images', 'extra/favicon.ico']
 EXTRA_PATH_METADATA = {
@@ -54,3 +60,5 @@ ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}.html'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}.html'
 PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
+
+DISPLAY_PAGES_ON_MENU = True
